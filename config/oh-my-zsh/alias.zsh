40483c121d8fc46ab2ef111fb0a84321a8825cad
kpavao84 <kpavao84@gmail.com>
2015-04-05 16:20:36 -0400

    initial

---
+++ config/oh-my-zsh/alias.zsh
@@ -0,0 +1,25 @@
+alias p='packer'
+alias ps='packer -S'
+alias psy='packer -Sy'
+alias psyu='packer -Syu'
+alias pss='packer -Ss'
+alias pac='sudo pacman'
+alias prsnd='sudo pacman -Rsnd'
+alias aur='packer --auronly'
+
+alias r='ranger'
+#alias i3lock= i3lock -i ~/wallpapers/yosemite.png
+
+alias server_start='sudo systemctl start nginx && sudo systemctl start php-fpm.service && sudo systemctl start mysqld.service'
+alias server_stop='sudo systemctl stop nginx && sudo systemctl stop php-fpm.service && sudo systemctl stop mysqld.service'
+alias server_restart='sudo systemctl restart nginx && sudo systemctl restart php-fpm.service && sudo systemctl restart mysqld.service'
+
+alias sushi='ssh kevin@66.228.62.98'
+#alias sushicp='scp kevin@66.228.62.98'
+alias sspi='ssh kevin@192.168.1.150'
+
+alias shutdown='sudo shutdown -h 0'
+
+alias hc='herbstclient'
+
+alias say='toilet --gay -f smmono12'
