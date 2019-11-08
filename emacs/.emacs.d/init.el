2700b4c54ae1c7d202bcaa5e0757eaaadee86056
Kevin Pavao <kevin.pavao@wellspring.com>
2019-11-08 11:05:42 -0600

    emacs - add some startup logic to init.el, taken from the doom faq

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,19 @@
-;;;;;;;;;;;;;;;;;
+;;; package --- Summary
 ;; ~/.emacs.d/init.el
-;;;;;;;;;;;;;;;;;
+
+;;; Commentary:
+;; This holds custom variables and loads emacs.org, where my actual config is
+
+;;; Code:
+
+;; -*- lexical-binding: t; -*-
+
+;; This is supposed to speed things up, according to the doom emacs FAQ:
+;; Emacs consults this variable every time a file is read or library loaded, or when certain functions in the file API are used (like expand-file-name or file-truename).
+;; They do so to check if a special handler is needed to read it, but none of these handlers are necessary for the initialization work we do at startup, so it is generally safe to disable it (temporarily!):
+;; It is important to restore this variable, otherwise you won’t be able to use TRAMP and Emacs will be unable to read compressed/encrypted files.
+(defvar doom--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -24,5 +37,9 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; Load the actual config file
 (org-babel-load-file "~/dotfiles/emacs.org")
+
+;; Then restore it later:
+(setq file-name-handler-alist doom--file-name-handler-alist)
+
+;;; init.el ends here
