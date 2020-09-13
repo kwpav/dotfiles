a6a343896745c61f04a789a011c70fa1cd83f9bd
Kevin Pavao <kpavao84@gmail.com>
2020-09-13 16:01:05 -0500

    emacs - add stuff from doom emacs to early-init.el, fix up doom-modeline a bit

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,15 +1,30 @@
 ;;; early-init.el --- -*- lexical-binding: t -*-
 
-;; (setq gc-cons-threshold 100000000)
-
 (setq gc-cons-threshold most-positive-fixnum)
 
 (setq package-enable-at-startup nil)
 
-(menu-bar-mode -99)
-(tool-bar-mode -1) ;; hide the toolbar
-(scroll-bar-mode -1) ;; hide the scrollbar
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Ignore X resources; its settings would be redundant with the other settings
+;; in this file and can conflict with later config (particularly where the
+;; cursor color is concerned).
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+(setq menu-bar-mode -99)
+;; (setq tool-bar-mode nil) ;; hide the toolbar
+;; (setq scroll-bar-mode nil) ;; hide the scrollbar
 (set-fringe-mode '(10 . 0)) ;; remove the extra border around frames
 (global-hl-line-mode 1) ;; highlight the current line
 
 (provide 'early-init)
+
+;;; early-init.el ends here
